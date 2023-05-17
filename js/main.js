1f3190164e3eba29f14025b9fa843b53da5b18a5
ShadePrima <aleksandrag1984@gmail.com>
2023-05-17 21:06:23 +0300

    slider

--- js/main.js
+++ js/main.js
@@ -2,3 +2,16 @@ AOS.init({
   // disabled: 'mobile',
   once: true,
 })
+
+//  initialize Swiper
+var swiper = new Swiper('.mySwiper', {
+  effect: 'coverflow',
+  grabCursor: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.button-arrow-right',
+    prevEl: '.button-arrow-left',
+  },
+})
